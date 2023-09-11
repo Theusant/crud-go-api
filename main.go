79362c3cf8f8c995130df6bb4b7969d6e220ee73
Matheus Santos <matheu.asssant@gmail.com>
2023-09-11 19:03:12 -0300

    routes test

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
+	"github.com/Theusant/crud-go-api/src/models/controllers/routes"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading")
+		log.Fatal("Error loading .env files")
 	}
 
-	fmt.Println(os.Getenv("Test"))
+	router := gin.Default()
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+
 }
--- src/models/controllers/createUser.go
+++ src/models/controllers/createUser.go
@@ -1,5 +1,11 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Theusant/crud-go-api/src/models/controllers/test/config/rest_err"
+	"github.com/gin-gonic/gin"
+)
 
-func CreateUser(c *gin.Context) {}
+func CreateUser(c *gin.Context) {
+	err := rest_err.NewBadRequestErr("Do you get routes bad request.")
+	c.JSON(err.Code, err)
+}
