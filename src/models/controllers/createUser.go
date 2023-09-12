c82efb4fe162a5a249ecdb37e424d9e80edff4a8
Matheus Santos <matheu.asssant@gmail.com>
2023-09-11 23:53:19 -0300

    user objects

--- src/models/controllers/createUser.go
+++ src/models/controllers/createUser.go
@@ -1,11 +1,23 @@
 package controllers
 
 import (
+	"fmt"
+
+	"github.com/Theusant/crud-go-api/src/models/controllers/models/requests"
 	"github.com/Theusant/crud-go-api/src/models/controllers/test/config/rest_err"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestErr("Do you get routes bad request.")
-	c.JSON(err.Code, err)
+
+	var UserResquest requests.UserResquest
+
+	if err := c.ShouldBindJSON(&UserResquest); err != nil {
+		restErr := rest_err.NewBadRequestErr(
+			fmt.Sprintf("There Some incorrect fields, error=%s\n", err.Error))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(UserResquest)
 }
